d50e559cfa1bfef744a94211bd8b9fa899b5a362
alvesat <fernandes.anttonio@gmail.com>
2021-04-19 18:34:51 -0300

    Exercicio Controle Repeticao

---
+++ ex_controle_repeticao.R
@@ -0,0 +1,29 @@
+# exercicio centralizacao 
+
+# base cars
+
+carro_base <- cars
+names(carro_base)
+
+# histograma da variavel "speed"
+
+hist(carro_base$speed)
+
+# centralizar a variavel "speed"
+
+speed <- carro_base$speed
+
+speed <- speed - mean(speed)
+
+hist(speed)
+
+# estrutura de repeticao para criacao dos histogramas
+
+par(mfrow = c(1,2))
+
+for (i in 1:2) {
+  x <- carro_base[,i]
+  hist(x,
+       main = paste("Variável", i, names(carro_base)[i]),
+       xlab = "Valores")
+}
