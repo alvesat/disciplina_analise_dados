11ecbeaf5d6ea730a2431d06170b887cc985b332
alvesat <36172849+alvesat@users.noreply.github.com>
2021-05-23 21:53:59 -0300

    Ex_2

---
+++ ex_2.R
@@ -0,0 +1,36 @@
+## exercicio 2 ##
+
+## lendo pacotes
+
+library(ff)
+library(readxl)
+
+## lendo dados
+
+setwd("C:/Users/55879/Documents/Eletiva_analise_dados/Eletiva_analise_dados/exercicio_2") ## definindo area
+
+situacao_final <- dir(pattern = "*.csv") ##List all files from the folder
+ldf <- list() ##Clista para o loop
+
+for (k in 1:length(situacao_final)){
+  ldf[[k]] <- read.csv(situacao_final[k],sep = ";")
+} 
+
+## convertendo para dataframe
+
+situacao_final_alunos <- do.call(rbind.data.frame, ldf)
+
+## convertendo para ff
+
+cols <- c(2,3,4,5,8,9,10,11,12,13, 15, 16, 17)
+
+situacao_final_alunos[cols] <- lapply(situacao_final_alunos[cols] , factor)
+
+banco <- as.ffdf(situacao_final_alunos)
+
+rm(a, ldf, b, class, cols, k, situacao_final)
+
+## salvando em rds
+
+local <- getwd()
+saveRDS(situacao_final_alunos, "banco.rds", local)
